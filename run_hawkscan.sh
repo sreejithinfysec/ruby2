5122ca20f542e736aa3e8093bf37564eab1d0da1
Adam Baldwin <adam.baldwin@stackhawk.com>
2020-10-25 21:02:33 -0600

    add readme and tweak script

--- run_hawkscan.sh
+++ run_hawkscan.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 
+port=${1:-3000}
+env=${2:-test}
+
 compose=$(which docker-compose)
 
-if [[ -f  ${compose} ]]; then 
+if [[ -f  ${compose} ]]; then
 	docker-compose build
-	docker-compose up -d 
-else 
+	docker-compose up -d
+else
+  docker network create hawknet
 	docker build -t stackhawk/vuln-graphql-ruby .
-	docker run --rm --name graphql-ruby -td -p 3000:3000 -e PORT=3000 -e ENV=test stackhawk/vuln-graphq-ruby
+	docker run --rm --name vuln-graphql-ruby --network hawknet -td -p ${port}:${port} -e PORT=${port} -e ENV=${env} stackhawk/vuln-graphq-ruby
 fi
 
 sleep 5
 
-source ./AUTH_TOKEN; 
+source ./AUTH_TOKEN;
 
-docker run -e AUTH_TOKEN="${token}" -e AUTH_COOKIE="${cookie}" -e SHAWK_DEBUG=true -e SHAWK_AUTH_ENDPOINT= -e APP_HOST=http://127.0.0.1:3000 --rm -v $(pwd):/hawk:rw -ti --name hawkscan --network hawknet stackhawk/hawkscan:latest
+docker run -e AUTH_TOKEN="${token}" -e AUTH_COOKIE="${cookie}" -e SHAWK_DEBUG=true -e SHAWK_AUTH_ENDPOINT= -e APP_HOST=http://127.0.0.1:${port} --rm -v $(pwd):/hawk:rw -ti --name hawkscan --network hawknet stackhawk/hawkscan:latest
 
 [[ $? != 0 ]] && exit 2
 
 if [[ -f ${compose} ]]; then
 	docker-compose down
-	docker-compose rm 
+	docker-compose rm
 else
 	docker kill vuln-graphql-ruby
 	docker rm vuln-graphq-ruby
+	docker network rm hawknet
 fi
