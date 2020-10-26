1d5628d99a73a9c2f1ea898228137d6bf38cda83
Adam Baldwin <adam.baldwin@stackhawk.com>
2020-10-26 12:44:01 -0600

    run script sans docker-compose

--- run_hawkscan.sh
+++ run_hawkscan.sh
@@ -3,16 +3,9 @@
 port=${1:-3000}
 env=${2:-test}
 
-compose=$(which docker-compose)
-
-if [[ -f  ${compose} ]]; then
-	docker-compose build
-	docker-compose up -d
-else
-  docker network create hawknet
-	docker build -t stackhawk/vuln-graphql-ruby .
-	docker run --rm --name vuln-graphql-ruby --network hawknet -td -p ${port}:${port} -e PORT=${port} -e ENV=${env} stackhawk/vuln-graphq-ruby
-fi
+docker network create hawknet
+docker build -t stackhawk/vuln-graphql-ruby .
+docker run --rm --name vuln-graphql-ruby --network hawknet -p ${port}:${port} -e PORT=${port} -e ENV=${env} stackhawk/vuln-graphql-ruby
 
 sleep 5
 
@@ -20,13 +13,7 @@ source ./AUTH_TOKEN;
 
 docker run -e AUTH_TOKEN="${token}" -e AUTH_COOKIE="${cookie}" -e SHAWK_DEBUG=true -e SHAWK_AUTH_ENDPOINT= -e APP_HOST=http://127.0.0.1:${port} --rm -v $(pwd):/hawk:rw -ti --name hawkscan --network hawknet stackhawk/hawkscan:latest
 
-[[ $? != 0 ]] && exit 2
+x=$?
+[[ ${x} != 0 ]] && exit ${x}
 
-if [[ -f ${compose} ]]; then
-	docker-compose down
-	docker-compose rm
-else
-	docker kill vuln-graphql-ruby
-	docker rm vuln-graphq-ruby
-	docker network rm hawknet
-fi
+docker kill vuln-graphql-ruby && docker rm vuln-graphq-ruby && docker network rm hawknet
